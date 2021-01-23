d5f5548131b60cc42c8c1e95e1d5f422cd83a453
Anatoly <kokotolik@gmail.com>
2021-01-23 15:57:49 +0300

    Make it better

--- bot.js
+++ bot.js
@@ -7,14 +7,8 @@ const bot = new VKBot({
     token : "72f8e0d9b6654a8ac278049b0dd4b99f573aa3b67d533d5a5b294ee543ed925003370fa304ffdbbd21c2d",
     confirmation : "1827676c"
 })
-bot.command((ctx)=>{
-    var message = ctx.message.text;
-    if (message = 'Толик'){
-        ctx.reply('старается')
-    }
-    else{
-        ctx.reply('Пудж')
-    }
+bot.on((ctx)=>{
+    ctx.reply('Ням')
 })
 
 const PORT = process.env.PORT || 80
