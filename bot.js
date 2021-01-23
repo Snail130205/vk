40d6ed506d414dd7b9080080ddf8623da9517dad
Anatoly <kokotolik@gmail.com>
2021-01-23 13:31:24 +0300

    Make it better

--- bot.js
+++ bot.js
@@ -1,9 +1,19 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const VKBot = require('node-vk-bot-api')
+
 const server = express()
+const bot = new VKBot({
+    token : "72f8e0d9b6654a8ac278049b0dd4b99f573aa3b67d533d5a5b294ee543ed925003370fa304ffdbbd21c2d",
+    confirmation : "b649c535"
+})
+bot.on((ctx)=>{
+    ctx.reply(ctx)
+})
 
 const PORT = process.env.PORT || 80
 
-
+server.use(bodyParser.json())
 
 server.listen(PORT, () =>{
     console.log('Server has been started')
