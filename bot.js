047fa91d010423dbb61eb57d0cd5d78cf02474f3
Anatoly <kokotolik@gmail.com>
2021-01-23 15:08:38 +0300

    Make it better

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ const VKBot = require('node-vk-bot-api')
 const server = express()
 const bot = new VKBot({
     token : "72f8e0d9b6654a8ac278049b0dd4b99f573aa3b67d533d5a5b294ee543ed925003370fa304ffdbbd21c2d",
-    confirmation : "b649c535"
+    confirmation : "1827676c"
 })
 bot.on((ctx)=>{
     var message = ctx.message.body;
