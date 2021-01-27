8a4095fe9830e68e38e8394d2a2b332fa9cb1b48
Anatoly <kokotolik@gmail.com>
2021-01-27 17:26:53 +0300

    Make it better

--- bot.js
+++ bot.js
@@ -3,8 +3,7 @@ const bodyParser = require('body-parser')
 const VKBot = require('node-vk-bot-api')
 
 const server = express()
-const bot = new VKBot('messages.getConversationsById',{
-    peer_ids: 1,
+const bot = new VKBot({
     token : "72f8e0d9b6654a8ac278049b0dd4b99f573aa3b67d533d5a5b294ee543ed925003370fa304ffdbbd21c2d",
     confirmation : "b6773308"
 });
