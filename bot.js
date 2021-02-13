dc1edb626c4a585e0e090f64a028e822f237e6dc
Anatoly <kokotolik@gmail.com>
2021-02-13 16:04:24 +0300

    Make it better

--- bot.js
+++ bot.js
@@ -3,17 +3,34 @@ const bodyParser = require('body-parser')
 const VKBot = require('node-vk-bot-api')
 var mysql = require('mysql')
 
-var connection = mysql.createConnection({
+var con = mysql.createConnection({
     host : "localhost",
     root : "root",
     password : "BotVk"
 })
+
+con.connect(function(err) {
+    if (err) throw err;
+})
+let dbws = 'No'
+con.connect(function(err) {
+    if (err) throw err;
+    dbws = 'yes'
+    con.query("CREATE DATABASE mydb", function (err, result) {
+        if (err) throw err;
+    });
+});
 
 const server = express()
 const bot = new VKBot({
     token : "72f8e0d9b6654a8ac278049b0dd4b99f573aa3b67d533d5a5b294ee543ed925003370fa304ffdbbd21c2d",
     confirmation : "93cc93d9"
 });
+
+
+bot.command('/db', (ctx) => {
+    ctx.reply(dbws)
+})
 
 // комманда помощь
 bot.command('/help', (ctx) => {
