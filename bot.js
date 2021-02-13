5bd7be6c83485281b25fe58af036ae5417012252
Anatoly <kokotolik@gmail.com>
2021-02-13 16:08:13 +0300

    Make it better

--- bot.js
+++ bot.js
@@ -9,18 +9,22 @@ var con = mysql.createConnection({
     password : "BotVk"
 })
 
-con.connect(function(err) {
-    if (err) throw err;
-})
 let dbws = 'No'
+
 con.connect(function(err) {
     if (err) throw err;
     dbws = 'yes'
+})
+
+
+/*
+con.connect(function(err) {
+    if (err) throw err;
     con.query("CREATE DATABASE mydb", function (err, result) {
         if (err) throw err;
     });
 });
-
+*/
 const server = express()
 const bot = new VKBot({
     token : "72f8e0d9b6654a8ac278049b0dd4b99f573aa3b67d533d5a5b294ee543ed925003370fa304ffdbbd21c2d",
